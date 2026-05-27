9de3b8483bbf3dac7a6ae1b4dc0f7df704ce1876
g9607111-web <g9607111@gmail.com>
2026-05-27 10:20:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -317,4 +317,4 @@ def delete_product(idx):
     return redirect("/admin")
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
